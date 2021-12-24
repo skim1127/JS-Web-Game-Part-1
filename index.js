d421b3f575e3186aec27b29a22283180a0140142
Jin mo Kim <skim112790@yahoo.com>
2021-12-24 15:09:03 -0500

    Finished web game activity part 1

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+    let element = document.createElement('img')
+    element.src = url
+    element.style.position = 'fixed'
+    element.style.left = left + 'px'
+    element.style.bottom = bottom + 'px'
+    document.body.append(element)
+    return element
+}
+
+function newItem(url, left, bottom) {
+    let element = newImage(url, left, bottom)
+
+    element.addEventListener('dblclick', function() {
+        element.remove()
+    })
+}
+
+newImage('./assets/red-character.gif', 200, 100)
+newImage('./assets/green-character.gif', 150, 100)
+newImage('./assets/pine-tree.png', 650, 200)
+newImage('./assets/tree.png', 200, 300)
+newImage('./assets/pillar.png', 10, 100)
+newImage('./assets/crate.png', 150, 200)
+newImage('./assets/well.png', 500, 425)
+
+newItem('./assets/sword.png', 500, 405)
+newItem('./assets/shield.png', 265, 185)
+newItem('./assets/staff.png', 600, 100)
